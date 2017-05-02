781fff08507ff4acde419cf5b1f5b9b1eb11614f
krysopath <krysopath@gmail.com>
2017-05-03 00:35:32 +0200

    0.1

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 # coding=utf-8
-"""
-Setupfile for safe
 
-"""
 from setuptools import setup
-
-out = setup(
+setup(
     name='safe',
-    version='0.1',
+    version='0.1.0',
     description='safe module, contains crypto related helpers',
-    url='ssh://git@endtropie.mooo.com:22222/home/git/safe.git',
     author='Georg',
     author_email='krysopath@gmail.com',
-    license='GPL',
+    url='ssh://git@endtropie.mooo.com:22222/home/git/safe.git',
     packages=['safe'],
-    install_requires=[
-        'gnupg',
-    ],
-    zip_safe=False
+    install_requires=['gnupg']
 )
